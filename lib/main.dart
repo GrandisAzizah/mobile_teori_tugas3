c5eac41c7e93ebccebf988a45d235b70fdb66338
nextnindcode <anindya.zahir04@gmail.com>
2026-09-18 02:02:02 +0700

    konfigurasi firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:mobile_teori_tugas3/screen/home_page.dart';
 
+import './firebase_options.dart';
 import './theme/app_theme.dart';
 import './screen/login_page.dart';
 import './screen/bantuan.dart';
 import './bottom_navigation/bottom_navigation.dart';
 import './screen/stopwatch.dart';
-import './screen/splash_page.dart'; // >>> TAMBAHAN <
+import './screen/splash_page.dart';
 import './screen/register_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -22,12 +26,11 @@ class MyApp extends StatelessWidget {
       title: 'EclipseOps',
       theme: AppTheme.light(),
       debugShowCheckedModeBanner: false,
-      // ini dibuat kayak gini supaya bottom nav bisa dipanggil
       initialRoute: '/login_page',
       routes: {
-        '/': (context) => const SplashPage(), // >>> TAMBAHAN <
+        '/': (context) => const SplashPage(),
         '/login_page': (context) => const LoginPage(),
-        '/register_page': (context) => const RegisterPage(), // >>> TAMBAHAN <
+        '/register_page': (context) => const RegisterPage(),
         '/main': (context) => const BottomNavigationPage(),
         '/stopwatch': (context) => const Stopwatch(),
         '/bantuan': (context) => const BantuanPage(),
